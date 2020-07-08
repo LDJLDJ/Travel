11a5f498e06767b2fd793a6e602f35e8ccd31a20
LDJ <1243754284@qq.com>
2020-07-08 14:45:34 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 // 这个是用于统一不同机型所显示的差异问题
-import './assets/styles/reset.css'
+import 'styles/reset.css'
 // 这个是用于多像素问题
-import './assets/styles/border.css'
+import 'styles/border.css'
 // 这个是用于一些移动端手机的小问题存在300毫秒延迟点击问题
 // cd Travel目录，npm install fastClick --save
 // 后面这个save可以在pakeage.json的dependencies中添加新配置
 import fastClick from 'fastClick'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
